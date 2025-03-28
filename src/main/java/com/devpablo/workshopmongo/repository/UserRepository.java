fe528591bf81db292c89d39347468bcc8a78a67b
Pablo Rodrigues <pablo.rodriguesbat@hotmail.com>
2025-03-28 13:39:20 -0300

    Operacao de instanciacao da base de dados

--- src/main/java/com/devpablo/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/devpablo/workshopmongo/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.devpablo.workshopmongo.repository;
 
 import com.devpablo.workshopmongo.domain.User;
+import java.util.List;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-    
+
+
 }
