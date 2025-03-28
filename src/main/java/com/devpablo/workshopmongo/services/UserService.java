9690477c6d043cfa23f76bfacaeada6e9c53e1e3
Pablo Rodrigues <pablo.rodriguesbat@hotmail.com>
2025-03-28 14:48:32 -0300

    Obtendo um usuario por id

--- src/main/java/com/devpablo/workshopmongo/services/UserService.java
+++ src/main/java/com/devpablo/workshopmongo/services/UserService.java
@@ -2,7 +2,9 @@ package com.devpablo.workshopmongo.services;
 
 import com.devpablo.workshopmongo.domain.User;
 import com.devpablo.workshopmongo.repository.UserRepository;
+import com.devpablo.workshopmongo.services.exception.ObjectNotFoundException;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +13,17 @@ public class UserService {
 
     @Autowired
     private UserRepository repo;
-    
+
     public List<User> findAll() {
         return repo.findAll();
     }
+
+    public User findById(String id) {
+        Optional<User> user = repo.findById(id);
+        if (!user.isPresent()) {
+            throw new ObjectNotFoundException("Objeto não encontrado! ID: " + id);
+        }
+        return user.get();
+    }
+
 }
