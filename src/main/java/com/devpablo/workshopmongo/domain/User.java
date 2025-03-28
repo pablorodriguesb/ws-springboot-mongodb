70a6bf0306055d8ecbb73fa7b147fd29ab5a3e59
Pablo Rodrigues <pablo.rodriguesbat@hotmail.com>
2025-03-28 11:55:58 -0300

    EntityUser e REST funcionando

---
+++ src/main/java/com/devpablo/workshopmongo/domain/User.java
@@ -0,0 +1,65 @@
+package com.devpablo.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+    private String id;
+    private String name;
+    private String email;
+    
+    public User() {
+    }
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        return Objects.equals(this.id, other.id);
+    }
+}
