192dc29b6863d72d40741781285f007666abfa26
Pablo Rodrigues <pablo.rodriguesbat@hotmail.com>
2025-03-29 10:24:07 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/devpablo/workshopmongo/services/UserService.java
+++ src/main/java/com/devpablo/workshopmongo/services/UserService.java
@@ -31,15 +31,27 @@ public class UserService {
         return repo.insert(obj);
     }
 
-    public User fromDTO(UserDTO objDto) {
-        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
-    }
-
     public void delete(String id) {
-        User obj = repo.findById(id).orElse(null);  
+        User obj = repo.findById(id).orElse(null);
         if (obj == null) {
             throw new ObjectNotFoundException("Usuário não encontrado com o ID: " + id);
         }
-        repo.delete(obj);  
+        repo.delete(obj);
+    }
+
+    public User update(User obj) {
+        User newObj = repo.findById(obj.getId())
+                .orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado com o ID: " + obj.getId()));
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
